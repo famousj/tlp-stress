8a6ae7461176e0ab552d18c98eb6ca8adb42332f
Jon Haddad <jon@jonhaddad.com>
2018-05-13 21:49:01 -0400

    working to get partition key generator working

--- src/main/kotlin/com/thelastpickle/tlpstress/PartitionKeyGenerator.kt
+++ src/main/kotlin/com/thelastpickle/tlpstress/PartitionKeyGenerator.kt
@@ -1,19 +1,35 @@
 package com.thelastpickle.tlpstress
 
+import java.util.concurrent.ThreadLocalRandom
+import kotlin.coroutines.experimental.buildSequence
+
 /**
- * We need to generate partition keys in every test
- * This class will manage the generation of partition keys
- * options:
- * sealed class
- * pass a generation algorithm + companions
+ * Accepts a function that generates numbers.
+ * returns a partition key using a prefix
+ * for any normal case there should be a compaction object function
+ * that should create a generator that has a function with all the logic inside it
  */
 
-class PartitionKeyGenerator(prefix: String) {
+class PartitionKeyGenerator(
+        val genFunc: () -> Int,
+        val prefix: String) {
     /**
      *
      */
-    fun generateNextKey(iteration: Int) : String {
-        return ""
+    companion object {
+        fun random(prefix: String = "test",
+                   max: Int = 1000000) : PartitionKeyGenerator {
+            return PartitionKeyGenerator({ThreadLocalRandom.current().nextInt(1, max) }, prefix)
+        }
+    }
+
+
+    fun generateKeys() = buildSequence {
+        while(true) {
+            val tmp = genFunc()
+            val result = prefix + tmp.toString()
+            yield(result)
+        }
     }
 
 }
--- src/test/kotlin/com/thelastpickle/PartitionKeyGeneratorTest.kt
+++ src/test/kotlin/com/thelastpickle/PartitionKeyGeneratorTest.kt
@@ -1,12 +1,15 @@
 package com.thelastpickle
 
 import com.thelastpickle.tlpstress.PartitionKeyGenerator
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 internal class PartitionKeyGeneratorTest {
     @Test
     fun basicKeyGenerationTest() {
-        var p = PartitionKeyGenerator("test")
-        p.generateNextKey(1)
+        val p = PartitionKeyGenerator.random("test")
+        val tmp = p.generateKeys()
+        val pk = tmp.take(1).toList()[0]
+        assertThat(pk).contains("test")
     }
 }
